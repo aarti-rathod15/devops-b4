bb75a6f2ecf20ff01cd202abb6f216e426659c4c
suraj <surajbele51@gmail.com>
2025-02-03 09:15:37 +0530

    terraform 1st file added

---
+++ instance.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = "us-east-1" 
+}
+resource "aws_instance" "myec2" {
+  ami = "ami-01816d07b1128cd2d"
+  key_name = "id_rsa"
+  instance_type =  t2.micro
+  vpc_security_group_ids = [ "mysg", "mysg2" ]
+  tags = {
+    Name = "spiderman instance"
+  }
+}
+
