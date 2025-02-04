fe831f3a310d6c7dffd71ac8573017bb459b1e56
suraj <surajbele51@gmail.com>
2025-02-04 08:33:11 +0530

    terraform file added

--- instance.tf
+++ instance.tf
@@ -4,7 +4,7 @@ provider "aws" {
 resource "aws_instance" "myec2" {
   ami = "ami-01816d07b1128cd2d"
   key_name = "id_rsa"
-  instance_type =  t2.micro
+  instance_type =  "t2.micro"
   vpc_security_group_ids = [ "mysg", "mysg2" ]
   tags = {
     Name = "spiderman instance"
