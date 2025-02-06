84be311a548790231d401ea417e20fe5aa41c2e7
aarti-rathod15 <aartirathod15501@gmail.com>
2025-02-07 00:20:07 +0530

    add terraform vpc setup

--- main.tf
+++ main.tf
@@ -1,35 +1,48 @@
-provider "aws" {
-  region = "us-east-1"
-}
-
-# Create VPC
-resource "aws_vpc" "my_vpc" {
+# main.tf
+resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
-  tags = {
-    Name = "MyVPC"
-  }
+  enable_dns_support = true
+  enable_dns_hostnames = true
 }
 
-# Create Subnet
-resource "aws_subnet" "my_subnet" {
-  vpc_id            = aws_vpc.my_vpc.id
-  cidr_block        = "10.0.1.0/24"
+resource "aws_subnet" "public" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
   availability_zone = "us-east-1a"
-  tags = {
-    Name = "MySubnet"
-  }
+  map_public_ip_on_launch = true
 }
 
-# Create Route Table
-resource "aws_route_table" "my_route_table" {
-  vpc_id = aws_vpc.my_vpc.id
-  tags = {
-    Name = "MyRouteTable"
-  }
+resource "aws_subnet" "private" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.2.0/24"
+  availability_zone = "us-east-1a"
 }
 
-# Associate Route Table with Subnet
-resource "aws_route_table_association" "my_association" {
-  subnet_id      = aws_subnet.my_subnet.id
-  route_table_id = aws_route_table.my_route_table.id
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_route" "internet_access" {
+  route_table_id         = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.main.id
+}
+
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_instance" "public" {
+  ami           = "ami-0c104f6f4a5d9d1d5" # Replace with your valid AMI ID for your region
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.public.id
+  associate_public_ip_address = true
+  tags = {
+    Name = "PublicInstance"
+  }
 }
